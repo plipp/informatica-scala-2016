83f671048331ebd0168b4efe2cbc50ab540f1f28
Patricia Lipp <p.lipp@web.de>
2016-06-08 09:50:20 +0200

    hello World

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,7 +1,11 @@
-// Tutorial Bot #1: Hello World
-
+// # Get Your First Bot Running
+//
+// 1. Open [the Bot Code File](../../src/main/scala/Bot.scala)
+// 2. Deploy it on the Gameboard as described in the [Bot-Development readme](../bot-development/readme.md)
+// 3. Change the response of the `ControlFunction` to "Hello World". Press 'r' on the board.
+//    Check on the gameboard, that the response text in fact changed.
 class ControlFunction {
-    def respond(input: String) = "Status(text=Hello)"
+    def respond(input: String) = "Status(text=Hello World)"
 }
 
 class ControlFunctionFactory {
