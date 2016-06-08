4efc45b176c97bf1f62b135d4db5069fb2c3a846
Patricia Lipp <p.lipp@web.de>
2016-06-08 10:04:08 +0200

    add a counter

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,7 +1,15 @@
-// Tutorial Bot #1: Hello World
+// Change the respond method so that it returns
+//    Hello Scala1
+//    Hello Scala2
+//    Hello Scala3 ...
 
 class ControlFunction {
-    def respond(input: String) = "Status(text=Hello)"
+    var cnt = 1
+    def respond(input: String): String = {
+        val status = s"Status(text=Hello Scala$cnt)"
+        cnt += 1
+        status
+    }
 }
 
 class ControlFunctionFactory {
