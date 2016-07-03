b8d1dba0e1fad29b5fdacd841df25630012725a4
plipp <p.lipp@web.de>
2016-07-03 20:10:54 +0200

    more docs

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -5,9 +5,18 @@
 // 3. Change the response of the `ControlFunction` to "Hello World". Press 'r' on the board.
 //    Check on the gameboard, that the response text in fact changed.
 class ControlFunction {
-    def respond(input: String) = "Status(text=Hello World)"
+
+    // The only door to the EXTERNAL world:
+    //
+    // Callback function, which is always called, when anything in the world around changes.
+    def respond(input: String): String = "Status(text=Hello World)"
 }
 
+
+// ----------------------------------------------------------------------------------
+// INTERNALS (you don't need to touch this during the workshop!)
+//
+// Entry Point for the Server
 class ControlFunctionFactory {
     def create = new ControlFunction().respond _
 }
