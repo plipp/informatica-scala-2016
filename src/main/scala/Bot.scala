ee9e4331311a9f4eca8cc239a5d89469b7e8daf5
plipp <p.lipp@web.de>
2016-07-03 20:10:54 +0200

    more docs

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,9 +1,18 @@
 // Tutorial Bot #1: Hello World
 
 class ControlFunction {
+
+    // The only door to the EXTERNAL world:
+    //
+    // Callback function, which is always called, when anything in the world around changes.
     def respond(input: String): String = "Status(text=Hello)"
 }
 
+
+// ----------------------------------------------------------------------------------
+// INTERNALS (you don't need to touch this during the workshop!)
+//
+// Entry Point for the Server
 class ControlFunctionFactory {
     def create = new ControlFunction().respond _
 }
