dd458bb54e52a45a5973b8318fc61b06170ebf55
Patricia Lipp <p.lipp@web.de>
2016-07-16 17:40:40 +0200

    solution

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -1,19 +1,19 @@
-// Tutorial Bot Class
+//2. Improve your Bot code:
+//  - Change your initial implementation of the ControlFunction#respond-method],
+//    so that it puts all parameters in a dictionary/map and fetches their values from there
 
 class ControlFunction {
-
-    // The only door to the EXTERNAL world:
-    //
-    // Callback function, which is always called, when anything in the world around changes.
-    def respond(input: String): String = "Status(text=Hello)"
+    def respond(input: String) = {
+        val tokens: Array[String] = input.split('(')
+        if (tokens(0)=="React") /* e.g. React(generation=0,time=0,view=__W_W_W__,energy=100) */ {
+            val params: Array[String] = tokens(1).dropRight(1).split(',')
+            val paramMap: Map[String, String] = params.map(param => param.split('=')).map(kv=>(kv(0),kv(1))).toMap
+            val energy = paramMap("energy")
+            s"Move(direction=0:-1)|Status(text=$energy)"
+        } else ""
+    }
 }
 
-
-// ----------------------------------------------------------------------------------
-// INTERNALS (you don't need to touch this during the workshop!)
-//
-// Entry Point for the Server
 class ControlFunctionFactory {
     def create = new ControlFunction().respond _
 }
-
